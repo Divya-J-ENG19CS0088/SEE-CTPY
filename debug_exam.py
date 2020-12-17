e46c4afa0c269bd1df09c3b685bf29a099772c8e
Divya-J-ENG19CS0088 <75435454+Divya-J-ENG19CS0088@users.noreply.github.com>
2020-12-17 13:40:06 +0530

    Update debug_exam.py

--- debug_exam.py
+++ debug_exam.py
@@ -112,3 +112,43 @@ if __name__ == '__main__':
     print(data1)
     print(data2)
     print(dup)
+5.a   
+Input
+3
+1 2
+2 2
+8 7
+2
+3 3
+4 4
+When key doesn't exits in data 1,then key value pair is not added to it. 
+b. 
+if k in dat 1:
+      v1 = data1[k]
+      if v1! = v2:
+      deepkeys [k] =[v1, v2]
+      del data1 [k]
+else :
+data 1[k]=v2
+c. 
+Test case 1:
+4
+1 2 
+3 3
+3 8
+4 9
+2
+3 3 
+4 4
+Test case 2:
+4
+1 2
+2 2
+3 3
+4 19
+2
+3 3
+4 19
+Test case 3:
+The test case written in 5.a , which breaks the initially written code can be written. 
+
